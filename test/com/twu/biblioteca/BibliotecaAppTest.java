b4bda192ec0a9bfb551cdce552abe84ec57a378f
karthikr-14 <karthik14081993@gmail.com>
2015-09-03 09:05:08 +0530

    initial

---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,17 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by karthikr on 03/09/15.
+ */
+public class BibliotecaAppTest {
+
+    @Test
+    public void abc() {
+        assertEquals(true, true);
+    }
+
+}
