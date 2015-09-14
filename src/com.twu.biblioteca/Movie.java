dd395585fc9f816ae8c7fc96ff8e9328a1b67819
karthikr-14 <karthik14081993@gmail.com>
2015-09-14 18:16:36 +0530

    Added Movie and Test Movie

---
+++ src/com.twu.biblioteca/Movie.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+public class Movie {
+
+    private String movieName;
+    private String directorName;
+    private int movieYear;
+    private float movieRating;
+
+    public Movie(String movieName, String directorName, int movieYear, float movieRating) {
+        this.movieName = movieName;
+        this.directorName = directorName;
+        this.movieYear = movieYear;
+        this.movieRating = movieRating;
+    }
+
+    public void displayMovie() {
+
+        System.out.println(String.format("%-40s%-25s%-25s%-25s", movieName, directorName, movieYear, movieRating));
+    }
+}
---
+++ test/TestMovie.java
@@ -0,0 +1,32 @@
+import com.twu.biblioteca.Movie;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMovie {
+
+    private final ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outputContent));
+    }
+
+    @After
+    public void CleanUpStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void shouldDisplayMovideDetails() {
+        Movie movie = new Movie("The Boy in the Striped pyjamas", "Mark Herman", 2008, 7.8f);
+        movie.displayMovie();
+
+        assertEquals(String.format("%-40s%-25s%-25s%-25s", "The Boy in the Striped pyjamas", "Mark Herman", 2008, 7.8)+"\n", outputContent.toString());
+    }
+}
