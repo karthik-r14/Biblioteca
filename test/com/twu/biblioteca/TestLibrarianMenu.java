10199b5081936e42a8c909a2983edd1c07c7ffaf
karthikr-14 <karthik14081993@gmail.com>
2015-09-16 16:52:23 +0530

    Added a spec in TestLibrarianMenu to validate checking out of a book

--- test/com/twu/biblioteca/TestLibrarianMenu.java
+++ test/com/twu/biblioteca/TestLibrarianMenu.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
@@ -68,5 +69,30 @@ public class TestLibrarianMenu {
         librarianMenu.executeOption("-1", library, new UserAccount("124-1234", "abc-defg", "user"));
         assertEquals("SELECT A VALID OPTION\n", outputContent.toString());
     }
+
+    @Test
+    public void shouldValidateCheckOutABook() {
+        String bookChoice = "One Night At the Call Center";
+        final ByteArrayInputStream inContent = new ByteArrayInputStream(bookChoice.getBytes());
+        System.setIn(inContent);
+
+        ArrayList<String> menu = new ArrayList<>();
+        LibrarianMenu librarianMenu = new LibrarianMenu(new UserMenu(menu));
+        librarianMenu.addOptions("List Books");
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("Five Point Someone", "Chetan Bhagat", 2004));
+        books.add(new Book("One Night At the Call Center", "Chetan Bhagat", 2005));
+        books.add(new Book("Revolution 2020", "Chetan Bhagat", 2011));
+
+        Library library = new Library(books, new ArrayList<Movie>());
+        librarianMenu.addOptions("List Books");
+        librarianMenu.addOptions("Checkout a Book");
+        librarianMenu.executeOption("2", library, new UserAccount("124-1234", "abc-defg", "user"));
+
+        assertEquals("ENTER BOOKNAME:\nThank you! Enjoy the book\n", outputContent.toString());
+    }
+
+    
 
 }
