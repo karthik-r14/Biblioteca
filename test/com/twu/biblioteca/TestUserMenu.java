231f7b7fe40b6ac2f23b2e16964e89784f67615b
karthikr-14 <karthik14081993@gmail.com>
2015-09-16 12:40:55 +0530

    Added a spec in TestUserMenu for validating return of a book

--- test/com/twu/biblioteca/TestUserMenu.java
+++ test/com/twu/biblioteca/TestUserMenu.java
@@ -88,4 +88,54 @@ public class TestUserMenu {
 
         assertEquals("ENTER BOOKNAME:\nThank you! Enjoy the book\n", outputContent.toString());
     }
+
+    @Test
+    public void shouldSuccessfullyReturnABookBack() {
+        String bookName = "Five Point Someone";
+        final ByteArrayInputStream inContent = new ByteArrayInputStream(bookName.getBytes());
+        System.setIn(inContent);
+
+        ArrayList<String> menu = new ArrayList<>();
+        UserMenu userMenu = new UserMenu(menu);
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("Five Point Someone", "Chetan Bhagat", 2004));
+        books.add(new Book("One Night At the Call Center", "Chetan Bhagat", 2005));
+        books.add(new Book("Revolution 2020", "Chetan Bhagat", 2011));
+
+        Library library = new Library(books, new ArrayList<Movie>());
+        library.checkoutABook("Five Point Someone");
+        userMenu.addOptions("List Books");
+        userMenu.addOptions("Checkout a Book");
+        userMenu.addOptions("Return a Book");
+
+        userMenu.executeOption("3", library, new UserAccount("124-1234", "abc-defg", "user"));
+
+        assertEquals("ENTER BOOK TO BE RETURNED:\nThank you for returning the book\n", outputContent.toString());
+    }
+
+    @Test
+    public void shouldNotifyOnUnsuccessfulReturnOfBook() {
+        String bookName = "2 States";
+        final ByteArrayInputStream inContent = new ByteArrayInputStream(bookName.getBytes());
+        System.setIn(inContent);
+
+        ArrayList<String> menu = new ArrayList<>();
+        UserMenu userMenu = new UserMenu(menu);
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("Five Point Someone", "Chetan Bhagat", 2004));
+        books.add(new Book("One Night At the Call Center", "Chetan Bhagat", 2005));
+        books.add(new Book("Revolution 2020", "Chetan Bhagat", 2011));
+
+        Library library = new Library(books, new ArrayList<Movie>());
+        library.checkoutABook("Five Point Someone");
+        userMenu.addOptions("List Books");
+        userMenu.addOptions("Checkout a Book");
+        userMenu.addOptions("Return a Book");
+        userMenu.executeOption("3", library, new UserAccount("124-1234", "abc-defg", "user"));
+
+        assertEquals("ENTER BOOK TO BE RETURNED:\nThat is not a valid book to return\n", outputContent.toString());
+    }
+
 }
